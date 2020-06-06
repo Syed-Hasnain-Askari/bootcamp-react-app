f1fb08ab4847fcf911c8222c31f3783c4858312d
Najaf Rizvi <hasnainaskari32@mail.com>
2020-06-06 17:22:22 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>Hellow World</h1>
+        <h1>Hellow World from Syed Hasnain Askari</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
